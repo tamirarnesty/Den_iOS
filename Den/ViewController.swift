9c0bf315210f42da0ed1e4d14374e3eb8a38d5ab
tamirsway <tamiryuval@live.ca>
2016-06-22 14:31:40 -0400

    Initial Commit

---
+++ Den/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Den
+//
+//  Created by Tamir Arnesty on 2016-06-22.
+//  Copyright © 2016 Tamir Arnesty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
